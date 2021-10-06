ebf4886b3488f5599089ee17845751ea08703805
Eldy Yuda Kurniawan <hardsnk11@gmail.com>
2021-10-06 17:02:13 +0800

    memperbaiki js

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1210,25 +1210,25 @@
   <!-- container-scroller -->
 
   <!-- plugins:js -->
-  <script src="{{asset('templete/vendors/js/vendor.bundle.base.js')}}"></script>
+  <script src="{{asset('template/vendors/js/vendor.bundle.base.js')}}"></script>
   <!-- endinject -->
   <!-- Plugin js for this page -->
-  <script src="{{asset('templete/vendors/chart.js/Chart.min.js')}}"></script>
-  <script src="{{asset('templete/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js')}}"></script>
-  <script src="{{asset('templete/vendors/progressbar.js/progressbar.min.js')}}"></script>
+  <script src="{{asset('template/vendors/chart.js/Chart.min.js')}}"></script>
+  <script src="{{asset('template/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js')}}"></script>
+  <script src="{{asset('template/vendors/progressbar.js/progressbar.min.js')}}"></script>
 
   <!-- End plugin js for this page -->
   <!-- inject:js -->
-  <script src="{{asset('templete/js/off-canvas.js')}}"></script>
-  <script src="{{asset('templete/js/hoverable-collapse.js')}}"></script>
-  <script src="{{asset('templete/js/template.js')}}"></script>
-  <script src="{{asset('templete/js/settings.js')}}"></script>
-  <script src="{{asset('templete/js/todolist.js')}}"></script>
+  <script src="{{asset('template/js/off-canvas.js')}}"></script>
+  <script src="{{asset('template/js/hoverable-collapse.js')}}"></script>
+  <script src="{{asset('template/js/template.js')}}"></script>
+  <script src="{{asset('template/js/settings.js')}}"></script>
+  <script src="{{asset('template/js/todolist.js')}}"></script>
 
   <!-- endinject -->
   <!-- Custom js for this page-->
-  <script src="{{asset('templete/js/dashboard.js')}}"></script>
-  <script src="{{asset('templete/js/Chart.roundedBarCharts.js')}}"></script>
+  <script src="{{asset('template/js/dashboard.js')}}"></script>
+  <script src="{{asset('template/js/Chart.roundedBarCharts.js')}}"></script>
   <!-- End custom js for this page-->
 </body>
 
